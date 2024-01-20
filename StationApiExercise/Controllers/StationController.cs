123829049069a5fd3f74a1f5a6fa07f13550a5f4
Lasse D. Skaalum <ldsk@trifork.com>
2024-01-21 00:09:12 +0100

    Remove test code

--- StationApiExercise/Controllers/StationController.cs
+++ StationApiExercise/Controllers/StationController.cs
@@ -1,7 +1,5 @@
 using StationApiExercise.Dtos;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StationApiExercise.Database;
 
 namespace StationApiExercise.Controllers;
 
@@ -12,7 +10,6 @@ public class StationController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<StationDto>>> GetStations()
     {
-        return Ok(await new StationDbContext().Stations.ToListAsync());
         // Just to make the compiler happy for now.
         return await Task.Run(() => Ok(new List<StationDto> { new(1, 123) }));
     }
